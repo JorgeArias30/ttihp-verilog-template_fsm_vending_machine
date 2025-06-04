661027e71b0424413ef249a218477cb7fd97f8d4
Jorge Arias <60588618+JorgeArias30@users.noreply.github.com>
2025-06-04 03:25:20 -0600

    tt_um_fsm_vending_machine_JorgeArias30.v

--- src/tt_um_fsm_vending_machine_JorgeArias30.v
+++ src/tt_um_fsm_vending_machine_JorgeArias30.v
@@ -29,7 +29,7 @@ module tt_um_fsm_vending_machine_JorgeArias30(
  wire [2:0] credit;
  wire       disp_sign;
 
-  wire [0]   moneda    = ui_in;
+//  wire [0]   moneda    = ui_in;
   wire [1]   aceptado  = ui_in;
   wire [2]   btnC      = ui_in;
   wire [3]   btnD      = ui_in;
